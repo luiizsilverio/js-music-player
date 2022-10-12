284979dcaa28c7b2cdf95d4e1828fa264198261c
luiizsilverio <luiiz.silverio@gmail.com>
2022-10-12 11:39:46 -0300

    First commit

---
+++ script.js
@@ -0,0 +1,86 @@
+const image = document.querySelector('img');
+const title = document.getElementById('title');
+const artist = document.getElementById('artist');
+const music = document.querySelector('audio');
+const prevBtn = document.getElementById('prev');
+const playBtn = document.getElementById('play');
+const nextBtn = document.getElementById('next');
+
+const songs  = [
+  {
+    name: 'metric-1',
+    displayName: 'Front Row (Remix)',
+    artist: 'Metric/Jacinto Design',
+  },
+  {
+    name: 'jacinto-1',
+    displayName: 'Electrict Chill Machine',
+    artist: 'Jacinto Design',
+  },
+  {
+    name: 'jacinto-2',
+    displayName: 'Seven Nation Army (Remix)',
+    artist: 'Jacinto Design',
+  },
+  {
+    name: 'jacinto-3',
+    displayName: 'Electrict Chill Machine',
+    artist: 'Jacinto Design',
+  },  
+]
+
+let isPlaying = false;
+let songIndex = 0;
+
+
+function playSong() {
+  isPlaying = true;
+  playBtn.classList.replace('fa-play', 'fa-pause');
+  playBtn.setAttribute('title', 'Pausar');
+  music.play();
+}
+
+function pauseSong() {
+  isPlaying = false;
+  playBtn.classList.replace('fa-pause', 'fa-play');
+  playBtn.setAttribute('title', 'Tocar');
+  music.pause();
+}
+
+function loadSong(song) {
+  title.textContent = song.displayName;
+  artist.textContent = song.artist;
+  music.src = `music/${song.name}.mp3`;
+  image.src = `img/${song.name}.jpg`;  
+}
+
+function nextSong() {
+  songIndex ++;
+  if (songIndex >= songs.length) {
+    songIndex = 0;
+  }
+  loadSong(songs[songIndex]);
+  playSong();
+}
+
+function prevSong() {
+  songIndex --;
+  if (songIndex < 0) {
+    songIndex = songs.length -1;
+  }
+  loadSong(songs[songIndex]);
+  playSong();
+}
+
+// Event Listeners
+
+playBtn.addEventListener('click', () => (
+  isPlaying ? pauseSong() : playSong()
+));
+
+prevBtn.addEventListener('click', prevSong);
+
+nextBtn.addEventListener('click', nextSong);
+
+// On Load - Select First Song
+loadSong(songs[songIndex]);
